7cff64f566bf1bfe0b2fa4fa489fc8f68d095b7e
Peter Ahinko <ahinko@users.noreply.github.com>
2022-05-16 22:32:30 +0200

    feat(ci): Automatically update allowed Kubernetes version (#12)
    * Downgrade versions so we can test the workflow
    
    * Add workflow to update allowed Kubernetes when Talos supports new version. This might require a lot of testing
    
    * Remove mac os x specific sed workaround
    
    * Update workflow
    
    * Fix path
    
    * More tweaks to workflow

---
+++ .github/scripts/kubernetes-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Find current Talos version .taskfiles/sidero.yaml
+TALOS_VERSION="$(sed -n 's/.*siderolabs\/installer:\([v0-9.]*\)/\1/p' .taskfiles/sidero.yaml)"
+
+# Get file from Talos repo that includes default Kubernetes version
+SOURCE_CONSTANTS=$(curl -s https://raw.githubusercontent.com/siderolabs/talos/$TALOS_VERSION/pkg/machinery/constants/constants.go)
+
+# Find kubernetes version
+TALOS_K8S_VERSION=$(expr "$SOURCE_CONSTANTS" : '.*DefaultKubernetesVersion = "\([0-9.]*\)"')
+
+# Concat to only include major.minor version
+ALLOWED_VERSION=$(expr "$TALOS_K8S_VERSION" : '\([0-9.]\{4\}\)')
+
+# Update renovate config
+sed -i "s/[0-9.]*\(', \/\/ Talos dependency\)/${ALLOWED_VERSION}\1/" .github/renovate/allowedVersions.json5
