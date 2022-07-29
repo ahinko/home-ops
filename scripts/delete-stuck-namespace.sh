cf604916b39255fd6a405bd91365bd07c4dbf8c3
Peter Ahinko <peter@ahinko.se>
2022-07-29 21:04:00 +0200

    Minor misc updates

--- scripts/delete-stuck-namespace.sh
+++ scripts/delete-stuck-namespace.sh
@@ -3,7 +3,7 @@
 function delete_namespace () {
     echo "Deleting namespace $1"
     kubectl get namespace $1 -o json > tmp.json
-    sed -i 's/"kubernetes"//g' tmp.json
+    gsed -i 's/"kubernetes"//g' tmp.json
     kubectl replace --raw "/api/v1/namespaces/$1/finalize" -f ./tmp.json
     rm ./tmp.json
 }
