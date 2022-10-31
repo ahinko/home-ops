d27fb9425425626fbc5077acc64639c7e7c784b5
Peter Ahinko <peter@ahinko.se>
2022-10-31 22:44:51 +0100

    Fix issues and tweaks to the Talos upgrade script

--- infrastructure/kubernetes/upgrade-talos.sh
+++ infrastructure/kubernetes/upgrade-talos.sh
@@ -6,7 +6,7 @@
 ROOK_CEPH_TOOLS_POD=$(kubectl get pod -l app=rook-ceph-tools -n rook-ceph -o jsonpath="{.items[0].metadata.name}")
 
 # make sure rook ceph is healthy
-HEALTH_CHECK=$(kubectl exec -n rook-ceph $ROOK_CEPH_TOOLS_POD -it ceph status | grep "health:")
+HEALTH_CHECK=$(kubectl exec -n rook-ceph $ROOK_CEPH_TOOLS_POD -- ceph status | grep "health:")
 
 if [[ $HEALTH_CHECK != *"HEALTH_OK"* ]]; then
   die "Make sure Rook/Ceph cluster is healthy since we determine by checking that when cycling through nodes"
@@ -24,24 +24,31 @@ do
 
     talosctl upgrade --preserve --wait -n $IP --image ghcr.io/siderolabs/installer:v1.2.6
 
+    # HACK: helios will hold up everything (because rook-ceph + controlplane) for up to 15 minutes until the taint has been removed.
+    if [[ $IP == "192.168.20.21" ]]; then
+      kubectl create job --from=cronjob/tainter -n kube-system tainter-temp if node is helios
+    fi
+
     HEALTH=false
     while [ "$HEALTH" == false ]
     do
-      echo "Rook/Ceph not healthy, waiting 60s before checking again."
-
-      # Sleep 60 seconds just to give Rook/Ceph time to get healthy
-      sleep 60
-
-      echo "Checking again..."
+      echo "Checking if Rook/Ceph is healthy."
 
       # Get name of rook ceph tools pod
       ROOK_CEPH_TOOLS_POD=$(kubectl get pod -l app=rook-ceph-tools -n rook-ceph -o jsonpath="{.items[0].metadata.name}")
 
       # make sure rook ceph is healthy
-      HEALTH_CHECK=$(kubectl exec -n rook-ceph $ROOK_CEPH_TOOLS_POD -it ceph status | grep "health:")
+      HEALTH_CHECK=$(kubectl exec -n rook-ceph $ROOK_CEPH_TOOLS_POD -- ceph status | grep "health:")
 
       if [[ $HEALTH_CHECK == *"HEALTH_OK"* ]]; then
         HEALTH=true
+      else
+        echo "Rook/Ceph not healthy, waiting 60s before checking again."
+        sleep 60
+
+        # Archive any crash messages since those will be holding up everything since the health will be false until those
+        # messages has been archived
+        kubectl exec -n rook-ceph $ROOK_CEPH_TOOLS_POD -- ceph crash archive-all
       fi
     done
 
