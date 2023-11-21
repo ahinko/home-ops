fbff39556eafd7b13c51472aae32d251f81a6194
Peter Ahinko <peter@ahinko.se>
2023-11-21 20:36:41 +0100

    Deploy .social domain and cloudflared

---
+++ infrastructure/terraform/cloudflare/domain_social.tf
@@ -0,0 +1,64 @@
+module "cf_domain_social" {
+  source     = "./modules/cf_domain"
+  domain     = data.sops_file.cloudflare_secrets.data["cloudflare_domain_social"]
+  account_id = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
+  dns_entries = [
+    #    {
+    #      id      = "www"
+    #      name    = "www"
+    #      value   = "ingress.${data.sops_file.cloudflare_secrets.data["cloudflare_domain_social"]}"
+    #      type    = "CNAME"
+    #      ttl     = 1
+    #      proxied = true
+    #    },
+    {
+      id    = "mx_no_mail"
+      name  = "@"
+      value = "."
+      type  = "MX"
+      ttl   = 1
+    },
+    {
+      id    = "dmarc_no_mail"
+      name  = "_dmarc"
+      value = "v=DMARC1;p=reject;sp=reject;adkim=s;aspf=s"
+      type  = "TXT"
+      ttl   = 1
+    },
+    {
+      id    = "domainkey_no_mail"
+      name  = "*._domainkey"
+      value = "v=DKIM1; p="
+      type  = "TXT"
+      ttl   = 1
+    },
+    {
+      id    = "spf1_no_mail"
+      name  = "@"
+      value = "v=spf1 -all"
+      type  = "TXT"
+      ttl   = 1
+    },
+  ]
+
+  waf_custom_rules = [
+    {
+      enabled     = true
+      description = "Firewall rule to block bots and threats determined by CF"
+      expression  = "(cf.client.bot) or (cf.threat_score gt 14)"
+      action      = "block"
+    },
+    {
+      enabled     = true
+      description = "Firewall rule to block all countries except SE/DK/NO/FI"
+      expression  = "(not ip.geoip.country in {\"SE\" \"DK\" \"NO\" \"FI\"})"
+      action      = "block"
+    },
+  ]
+}
+
+resource "cloudflare_tunnel" "social" {
+  account_id = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
+  name       = "social"
+  secret     = base64encode(data.sops_file.cloudflare_secrets.data["cloudflare_social_tunnel_secret"])
+}
