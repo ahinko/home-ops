fdcf2e6bcae79e225a48878d5887ab8693c6660b
Peter Ahinko <peter@ahinko.se>
2024-08-01 22:10:34 +0200

    feat(samba): move from docker to storage cluster

---
+++ backups/backup_to_external_disk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+kubectl config use-context storage
+kubectl apply -f ./deployment.yaml
+
+POD=$(kubectl get pod -l app=zfs-backup -o jsonpath="{.items[0].metadata.name}")
+
+DATASETS=( minio samba immich/library )
+TARGET_PATH=/Volumes/Backup
+
+# Loop through datasets
+for dataset in "${DATASETS[@]}";
+do
+        echo "Rysnc ${dataset} to ${TARGET_PATH}"
+        kubectl rsync -- -a -v --progress --delete ${POD}:/backup/${dataset} ${TARGET_PATH}
+done
+
+kubectl delete -f ./deployment.yaml
+
+echo "Backup done!"
