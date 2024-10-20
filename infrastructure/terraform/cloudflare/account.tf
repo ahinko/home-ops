f543836dd34f78a2a5b7302187af119e1ed9cc92
Peter Ahinko <peter@ahinko.se>
2024-10-20 17:36:13 +0200

    fix(terraform): remove

--- infrastructure/terraform/cloudflare/account.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_account" "ahinko" {
-  name              = "My Homelab"
-  type              = "standard"
-  enforce_twofactor = true
-}
--- infrastructure/terraform/cloudflare/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  cloudflare_secrets = sensitive(yamldecode(nonsensitive(data.sops_file.cloudflare_secrets.raw)))
-  home_ipv4          = chomp(data.http.ipv4.response_body)
-}
