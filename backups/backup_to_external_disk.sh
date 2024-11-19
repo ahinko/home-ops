2235c8f0a225ca496376171a48f65f217eccc44d
Peter Ahinko <peter@ahinko.se>
2024-11-19 20:05:04 +0100

    fix(backup): fix broken backup script

--- backups/backup_to_external_disk.sh
+++ backups/backup_to_external_disk.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-kubectl config use-context storage
+kubectl config use-context main
 kubectl apply -f ./deployment.yaml
 
 echo "Waiting for pod..."
-sleep 15
+sleep 3
 
 POD=$(kubectl get pod -l app=zfs-backup -o jsonpath="{.items[0].metadata.name}")
+
+kubectl wait --for=condition=Ready pod/${POD}
 
 DATASETS=( minio samba immich/library )
 TARGET_PATH=/Volumes/Backup
@@ -17,6 +19,8 @@ do
         echo "Rysnc ${dataset} to ${TARGET_PATH}"
         kubectl rsync -- -a -v --progress --delete ${POD}:/backup/${dataset} ${TARGET_PATH}
 done
+
+sleep 15
 
 kubectl delete -f ./deployment.yaml
 
